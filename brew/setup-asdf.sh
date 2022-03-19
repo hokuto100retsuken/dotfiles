04fcfa925fbdc9ac5d93c4e09a243ed92c431802
hkt100rtkn <hokuto100retsuken@gmail.com>
2022-03-19 23:12:49 +0900

    add brew setup files

---
+++ brew/setup-asdf.sh
@@ -0,0 +1,12 @@
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin add python
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin add terraform
+asdf plugin add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git
+
+asdf install ruby 3.1.1
+asdf install nodejs 16.14.0
+asdf install python 3.8.11
+asdf install golang 1.16.7
+# その他必要に応じてインストール

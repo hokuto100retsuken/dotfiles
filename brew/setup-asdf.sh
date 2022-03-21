9d068da775ed1a0e57b1fa9448f3f660bc0ac2e6
hkt100rtkn <hokuto100retsuken@gmail.com>
2022-03-21 15:18:38 +0900

    add asdf rust

--- brew/setup-asdf.sh
+++ brew/setup-asdf.sh
@@ -4,9 +4,11 @@ asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add golang https://github.com/kennyp/asdf-golang.git
 asdf plugin add terraform
 asdf plugin add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git
+asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git
 
 asdf install ruby 3.1.1
 asdf install nodejs 16.14.0
 asdf install python 3.8.11
 asdf install golang 1.16.7
+asdf install rust 1.59.0
 # その他必要に応じてインストール

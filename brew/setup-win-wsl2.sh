75d5a789ec11efe49383dd4748771eab64d2ea20
hkt100rtkn <hokuto100retsuken@gmail.com>
2022-06-03 01:15:44 +0900

    create win wsl2 setup file

---
+++ brew/setup-win-wsl2.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+# zsh install
+sudo apt-get install zsh
+chsh -s $(which zsh)
+
+touch ~/.zprofile
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.zprofile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+brew install peco
+brew install asdf

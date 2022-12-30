56dfb205f4d4a2e19d76ed19f676bd7be00ae72f
hkt100rtkn <hokuto100retsuken@gmail.com>
2022-12-31 02:07:36 +0900

    nvimの設定を追加

---
+++ dotfiles/_config/nvim/lua/hkt100rtkn/plugins/comment.lua
@@ -0,0 +1,8 @@
+-- import comment pluging safely
+local setup, comment = pcall(require, 'Comment')
+if not setup then
+  return
+end
+-- enable comment
+comment.setup()
+
--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -3,3 +3,7 @@
 ln -sfv `pwd`/_zshrc ~/.zshrc
 ln -sfv `pwd`/_config/starship.toml ~/.config/starship.toml
 
+# nvim setting 
+ln -sfv `pwd`/_config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sfv `pwd`/_config/nvim/lua ~/.config/nvim/
+

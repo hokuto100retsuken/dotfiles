78250a4aebb77fab0472130a34ac46e67c1bddab
愛内 一浩 <hokuto100retsuken@pepabo.com>
2023-06-15 15:04:17 +0900

    nvimの設定を消した

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 DOTPATH=$(pwd)
-CONFIG_PATH=~/.config/nvim
-
-if [ ! -d "$CONFIG_PATH" ]; then
-  mkdir -p ~/.config/nvim
-fi
 
 ln -sfv $DOTPATH/dotfiles/_zshrc ~/.zshrc
 ln -sfv $DOTPATH/dotfiles/_config/starship.toml ~/.config/starship.toml
-
-# nvim setting 
-ln -sfv $DOTPATH/dotfiles/_config/nvim/init.lua ~/.config/nvim/init.lua
-ln -sfv $DOTPATH/dotfiles/_config/nvim/lua ~/.config/nvim/
 
 # tmux config
 ln -sfv $DOTPATH/dotfiles/_tmux.conf ~/.tmux.conf

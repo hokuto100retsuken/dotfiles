ebcfc9ae1e5c7eda82547577126e194e7d02bf85
hkt100rtkn <hokuto100retsuken@gmail.com>
2023-09-15 01:59:38 +0900

    change sheldon

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -3,6 +3,9 @@
 DOTPATH=$(pwd)
 
 ln -sfv $DOTPATH/dotfiles/_zshrc ~/.zshrc
+# ~/.config/sheldon/ のフォルダがないなら作成
+[ -d ~/.config/sheldon ] || mkdir -p ~/.config/sheldon
+ln -sfv $DOTPATH/dotfiles/_config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
 ln -sfv $DOTPATH/dotfiles/_config/starship.toml ~/.config/starship.toml
 
 # tmux config

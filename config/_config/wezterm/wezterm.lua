6e3a41f6d8c90f62622569ab8a7ada638849fb98
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-01-28 03:06:13 +0900

    weztermの設定を追加

---
+++ config/_config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+-- require 'keybindings'
+require 'format'
+require 'status'
+
+return {
+    color_scheme = 'Belge (terminal.sexy)',
+    window_background_opacity = 0.93,
+    window_frame = {
+        font_size = 8.0,
+    },
+    status_update_interval = 1000,
+}
--- config/install.sh
+++ config/install.sh
@@ -4,6 +4,7 @@
 # ~/.config/sheldon/ のフォルダがないなら作成
 [ -d ~/.config/sheldon ] || mkdir -p ~/.config/sheldon
 ln -sfv $PWD/_config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+ln -sfv $PWD/_config/wezterm ~/.config/wezterm
 
 if [[ ! -e ~/.config/starship.toml ]]; then
   ln -sfv $PWD/starship.toml ~/.config/starship.toml

5213d084de094d1cfe5b35a974b1a532be2a481f
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-01-29 01:31:30 +0900

    nvimの設定 その1

---
+++ nvim/lua/options.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_set_option('termguicolors', true)
+vim.api.nvim_set_option('scrolloff', 4)
+
+-- find
+vim.api.nvim_set_option('ignorecase', true)
+vim.api.nvim_set_option('smartcase', true)
+
+vim.api.nvim_set_option('inccommand', 'split')
+vim.api.nvim_set_option('clipboard', 'unnamedplus')
+

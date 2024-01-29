9fdd4cf348a9121ee870707faf7c670a2379cc7b
aibo <hokuto100retsuken@pepabo.com>
2024-01-29 15:09:20 +0900

    サイズ変更

--- config/_config/wezterm/wezterm.lua
+++ config/_config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ return {
     color_scheme = 'Belge (terminal.sexy)',
     window_background_opacity = 0.93,
     window_frame = {
-        font_size = 8.0,
+        font_size = 12.0,
     },
     status_update_interval = 1000,
 }

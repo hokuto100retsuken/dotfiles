59fbb6ba074d184b6479e970a331a050f75a4d25
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-02-03 16:13:51 +0900

    nvimの最初の一歩

--- config/install.sh
+++ config/install.sh
@@ -4,7 +4,12 @@
 # ~/.config/sheldon/ のフォルダがないなら作成
 [ -d ~/.config/sheldon ] || mkdir -p ~/.config/sheldon
 ln -sfv $PWD/_config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+
+[ -d ~/.config/wezterm ] || mv ~/.config/wezterm ~/.config/wezterm.bak
 ln -sfv $PWD/_config/wezterm/ ~/.config/wezterm
+
+[ -d ~/config/nvim ] || mv ~/.config/nvim ~/.config/nvim.bak
+ln -sfv $PWD/_config/nvim ~/.config/nvim
 
 if [[ ! -e ~/.config/starship.toml ]]; then
   ln -sfv $PWD/starship.toml ~/.config/starship.toml

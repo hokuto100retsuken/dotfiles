5dca6ae083d4220a6cf0c7faf7c33b69a5f39248
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-02-04 02:02:37 +0900

    configフォルダの整理

--- config/install.sh
+++ config/install.sh
@@ -3,13 +3,13 @@
 ## sheldon
 # ~/.config/sheldon/ のフォルダがないなら作成
 [ -d ~/.config/sheldon ] || mkdir -p ~/.config/sheldon
-ln -sfv $PWD/_config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+ln -sfv $PWD/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
 
 [ -d ~/.config/wezterm ] || mv ~/.config/wezterm ~/.config/wezterm.bak
-ln -sfv $PWD/_config/wezterm/ ~/.config/wezterm
+ln -sfv $PWD/wezterm/ ~/.config/wezterm
 
 [ -d ~/config/nvim ] || mv ~/.config/nvim ~/.config/nvim.bak
-ln -sfv $PWD/_config/nvim ~/.config/nvim
+ln -sfv $PWD/nvim ~/.config/nvim
 
 if [[ ! -e ~/.config/starship.toml ]]; then
   ln -sfv $PWD/starship.toml ~/.config/starship.toml

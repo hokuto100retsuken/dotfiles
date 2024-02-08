f2b0979b17846c0038f016c551bbd9a70ab80a92
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-02-09 02:30:18 +0900

    copilot追加

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+local utils = require('utils')
+local imap = utils.keymaps.imap
+
+local config = function()
+  vim.g['copilot_no_tab_map'] = 1
+  imap('<Plug>(vimrc:copilot-dummy-map)', 'copilot#Accept("\\<Tab>")', { expr = true })
+end
+
+local copilot = {
+  'github/copilot.vim',
+  event = { 'BufRead', 'BufNewFile' },
+  config = config
+}
+
+return copilot

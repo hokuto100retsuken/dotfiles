ba2cd97b28946e11253e5f08fdd1d593759354fb
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-02-13 01:34:09 +0900

    add sonictemplate

--- config/nvim/lua/plugins/list.lua
+++ config/nvim/lua/plugins/list.lua
@@ -1,16 +1,21 @@
 local list = {
     -- colorscheme
-    'colorscheme/colorscheme',
+    'colorscheme/nvchad-colorizer',
+    'colorscheme/nightfox',
 
     -- completion
     'completion/copilot',
+    'completion/emmet',
     'completion/formatting',
     'completion/linting',
     'completion/nvim-cmp',
     'completion/nvim-surround',
+    'completion/sonictemplate',
 
     -- filer
     'filer/nvim-tree',
+    -- 'filer/fern-renderer-nerdfont',
+    -- 'filer/fern',
 
     -- fuzzy finder
     'fuzzyfinder/telescope',

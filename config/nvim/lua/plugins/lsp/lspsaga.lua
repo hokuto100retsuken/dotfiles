a346cfffeac4655bd240e9eb739c40185b2592d5
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-02-26 00:28:51 +0900

    nvim update

---
+++ config/nvim/lua/plugins/lsp/lspsaga.lua
@@ -0,0 +1,12 @@
+local lapsaga = {
+    'nvimdev/lspsaga.nvim',
+    config = function()
+        require('lspsaga').setup({})
+    end,
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter', -- optional
+        'nvim-tree/nvim-web-devicons'     -- optional
+    },
+}
+
+return lapsaga

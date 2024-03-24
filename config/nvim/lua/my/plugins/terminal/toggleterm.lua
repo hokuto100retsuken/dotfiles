ff90d07db6288ae187537ff23790436dc7a730fd
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-03-25 00:29:57 +0900

    toggletermを追加

---
+++ config/nvim/lua/my/plugins/terminal/toggleterm.lua
@@ -0,0 +1,24 @@
+local utils = require('my/utils')
+local nmap = utils.keymaps.nmap
+
+local toggleterm = {
+    "akinsho/nvim-toggleterm.lua",
+    config = function()
+        local t = require("toggleterm")
+        t.setup{
+            size = 20,
+            open_mapping = [[<c-\>]],
+            hide_numbers = true,
+            shade_filetypes = {},
+            shade_terminals = true,
+            shading_factor = 2,
+            start_in_insert = true,
+            persist_size = true,
+            direction = 'float',
+            close_on_exit = true,
+            shell = vim.o.shell,
+        }
+    end,
+}
+
+return toggleterm

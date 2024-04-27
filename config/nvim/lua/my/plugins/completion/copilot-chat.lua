57521985d3403e142a57d1994956f10858848996
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-04-27 15:23:22 +0900

    Telescopeを使えるようにした

--- config/nvim/lua/my/plugins/completion/copilot-chat.lua
+++ config/nvim/lua/my/plugins/completion/copilot-chat.lua
@@ -1,15 +1,26 @@
+-- telescope を使ってアクションプロンプトを表示する
+function ShowCopilotChatActionPrompt()
+  local actions = require("CopilotChat.actions")
+  require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+end
+
+-- キーマッピング
+-- <leader>ccp (Copilot Chat Prompt の略) でアクションプロンプトを表示する
+vim.api.nvim_set_keymap("n", "<leader>ccp", "<cmd>lua ShowCopilotChatActionPrompt()<cr>",
+  { noremap = true, silent = true })
+
 return {
-    {
-      "CopilotC-Nvim/CopilotChat.nvim",
-      branch = "canary",
-      dependencies = {
-        { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-        { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-      },
-      opts = {
-        debug = true, -- Enable debugging
-        -- See Configuration section for rest
-      },
-      -- See Commands section for default commands if you want to lazy load on them
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },   -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },    -- for curl, log wrapper
     },
-  }
+    opts = {
+      debug = true,   -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}

55a188d90e95f639ae11ebe5d1bde7c39a1cfce3
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-05-04 18:11:44 +0900

    update

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -7,12 +7,12 @@ ln -sfv $PWD/zsh/_zshrc ~/.zshrc
 ## sheldon
 # ~/.config/sheldon/ のフォルダがないなら作成
 [ -d ~/.config/sheldon ] || mkdir -p ~/.config/sheldon
-ln -sfv $PWD/config/_config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+ln -sfv $PWD/config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
 
 ## nvim
 # ~/.config/astronvim/lua/user/plugins のフォルダがないなら作成
 [ -d ~/.config/astronvim/lua/user/plugins ] || mkdir -p ~/.config/astronvim/lua/user/plugins
-ln -sfv $PWD/config/_config/astronvim/lua/user/plugins/user.lua ~/.config/astronvim/lua/user/plugins/user.lua
+ln -sfv $PWD/config/astronvim/lua/user/plugins/user.lua ~/.config/astronvim/lua/user/plugins/user.lua
 # ln -sfv $PWD/dotfiles/_config/starship.toml ~/.config/starship.toml
 
 # tmux config

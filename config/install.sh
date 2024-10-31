5b41f8960d513cfe8f71cb839f92f75e3a2f4640
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-10-31 09:07:01 +0900

    fish installer

--- config/install.sh
+++ config/install.sh
@@ -6,18 +6,18 @@
 ln -sfv $PWD/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
 
 
-if [ -d ~/.config/wezterm ]; then
-  mv ~/.config/wezterm ~/.config/wezterm.bak
-fi
+# if [ -d ~/.config/wezterm ]; then
+#   mv ~/.config/wezterm ~/.config/wezterm.bak
+# fi
 ln -sfv $PWD/wezterm/ ~/.config/wezterm
 
-if [ -d ~/config/nvim ]; then
-   mv ~/.config/nvim ~/.config/nvim.bak
-fi
+# if [ -d ~/config/nvim ]; then
+#    mv ~/.config/nvim ~/.config/nvim.bak
+# fi
 ln -sfv $PWD/nvim ~/.config/nvim
 
-if [ ! -e ~/.config/starship.toml ]; then
-  ln -sfv $PWD/starship.toml ~/.config/starship.toml
-fi
+# if [ ! -e ~/.config/starship.toml ]; then
+#   ln -sfv $PWD/starship.toml ~/.config/starship.toml
+# fi
 
 ln -sfv $PWD/fish ~/.config/fish

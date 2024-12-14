0e05081623b79df111e5d9c5a471c01bdd500ec2
hkt100rtkn <hokuto100retsuken@gmail.com>
2024-12-14 21:32:33 +0900

    変更を追加

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,9 +7,9 @@ set -g theme_color_scheme dark
 #path
 set -x PATH $HOME/.nodebrew/current/bin $PATH
 
-set -x PYENV_ROOT $HOME/.pyenv
-set -x PATH  $PYENV_ROOT/bin $PATH
-pyenv init - | source
+# set -x PYENV_ROOT $HOME/.pyenv
+# set -x PATH  $PYENV_ROOT/bin $PATH
+# pyenv init - | source
 
 #peco setting
 set fish_plugins theme peco
@@ -19,3 +19,22 @@ function fish_user_key_bindings
 end
 
 starship init fish | source
+
+# source ~/.asdf/asdf.fish
+
+# alias docker-compose 'docker compose'
+alias docker-compose="docker compose"
+
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+function list_git_branches
+    if test -d .git
+        set branch (git branch -a | fzf | sed 's/^[* ] //')
+        git checkout $branch
+    else
+        echo "This is not a Git repository."
+    end
+end
+
+bind \cb 'list_git_branches'
+

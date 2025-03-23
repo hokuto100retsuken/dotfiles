060819605bb2f457023119e2176d0dd242eef7ce
hkt100rtkn <hokuto100retsuken@gmail.com>
2025-03-23 20:07:58 +0900

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,17 +3,3 @@ set -g theme_display_date yes
 set -g theme_date_format "+%F %H:%M"
 set -g theme_display_git_default_branch yes
 set -g theme_color_scheme dark
-
-#path
-set -x PATH $HOME/.nodebrew/current/bin $PATH
-
-# peco setting
-set fish_plugins theme peco
-
-starship init fish | source
-
-# alias docker-compose 'docker compose'
-alias docker-compose="docker compose"
-
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-

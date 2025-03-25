7cb2e9164b686c39c3bdc1fe8a9c3338358f8e90
hkt100rtkn <hokuto100retsuken@gmail.com>
2025-03-25 14:57:02 +0900

    update fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,13 +4,20 @@ set -g theme_date_format "+%F %H:%M"
 set -g theme_display_git_default_branch yes
 set -g theme_color_scheme dark
 
-#  functions
-function pbr
+# ghq select option
+set -g GHQ_SELECTOR fzf
+set -g GHQ_SELECTOR_OPTS --reverse --height=40% --ansi --color bg+:13,hl:3,pointer:7
+
+# functions
+function switch_branch
+    # 入力欄の再描画（fzf開始前に実行）
+    commandline -f repaint
+
     # Git ブランチの一覧を取得（現在のブランチには * が付いているので除去）
     set branches (git branch --format="%(refname:short)")
     if test (count $branches) -gt 0
-        # peco で選択
-        set branch (string join \n $branches | peco)
+        # fzf で選択（画面全体を消さず、入力下に表示）
+        set branch (string join \n $branches | fzf --height 20% --border --ansi)
         if test -n "$branch"
             set -l output (git checkout $branch 2>&1)
             set -l exit_status $status
@@ -18,13 +25,15 @@ function pbr
             if test $exit_status -ne 0
                 echo "ブランチの切り替えに失敗しました。"
             end
-            commandline -f repaint
         end
     else
         echo "No branches found."
-        commandline -f repaint
     end
+
+    # 入力欄の再描画（fzf終了後に実行）
+    commandline -f repaint
 end
 
-#  key bind
-bind \cb 'pbr'
+# key bind
+bind \cb 'switch_branch'
+

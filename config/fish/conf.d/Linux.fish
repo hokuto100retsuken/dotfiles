131cfe5ea6ff88e6cda0d7f8a96678841ce13943
hkt100rtkn <hokuto100retsuken@gmail.com>
2025-07-02 23:41:35 +0900

    update

--- config/fish/conf.d/Linux.fish
+++ config/fish/conf.d/Linux.fish
@@ -1,10 +1,1 @@
-
-# /home/hkt100rtkn/.local/bin/mise activate fish | source
-
-# mise
-set -Ux MISE_EXPERIMENTAL 1
 /home/hkt100rtkn/.local/bin/mise activate fish | source
-
-# aqua
-set -x AQUA_ROOT_DIR $HOME/.aqua
-set -x PATH $AQUA_ROOT_DIR/bin $PATH
--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -11,17 +11,17 @@ ln -sfv $PWD/config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
 ln -sfv $PWD/tmux/_tmux.conf ~/.tmux.conf
 
 # git config
-cp $PWD/git/_gitconfig ~/.gitconfig
+ln -sfv $PWD/git/_gitconfig ~/.gitconfig
 
 # fish
-[ -d ~/.config/fish/functions ] || mkdir -p ~/.config/fish/functions
+[ -d ~/.config/fish ] || mkdir -p ~/.config/fish
 ln -sfv $PWD/config/fish/config.fish ~/.config/fish/config.fish
+ln -sfv $PWD/config/fish/conf.d ~/.config/fish/conf.d
 ln -sfv $PWD/config/fish/functions ~/.config/fish/functions
-
-# mise
-[ -d ~/.config/mise ] || mkdir -p ~/.config/mise
-ln -sfv $PWD/config/mise/config.toml ~/.config/mise/config.toml
 
 # aqua
 [ -d ~/.config/aqua ] || mkdir -p ~/.config/aqua
 ln -sfv $PWD/config/aqua/aqua.yaml ~/.config/aqua/aqua.yaml
+
+# Install aqua packages
+aqua i

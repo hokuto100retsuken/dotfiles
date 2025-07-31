8034781d6119dca6e854936a027547d416f64430
aibo <hokuto100retsuken@pepabo.com>
2025-07-31 17:54:54 +0900

    リンクの仕方を変更

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -2,13 +2,8 @@
 
 DOTPATH=$(pwd)
 
-## sheldon
-# ~/.config/sheldon/ のフォルダがないなら作成
-[ -d ~/.config/sheldon ] || mkdir -p ~/.config/sheldon
-ln -sfv $PWD/config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
-
-# tmux config
-ln -sfv $PWD/tmux/_tmux.conf ~/.tmux.conf
+# starship
+ln -sfv $PWD/config/starship.toml ~/.config/starship.toml
 
 # git config
 ln -sfv $PWD/git/_gitconfig ~/.gitconfig
@@ -18,4 +13,18 @@ ln -sfv $PWD/git/_gitconfig ~/.gitconfig
 ln -sfv $PWD/config/fish/config.fish ~/.config/fish/config.fish
 ln -sfv $PWD/config/fish/conf.d ~/.config/fish/conf.d
 ln -sfv $PWD/config/fish/functions ~/.config/fish/functions
+
+# ghostty
+[ -d ~/.config/ghostty ] || mkdir -p ~/.config/ghostty
+ln -sfv $PWD/config/ghostty/config ~/.config/ghostty/config
+
+# mise
+[ -d ~/.config/mise ] || mkdir -p ~/.config/mise
+ln -sfv $PWD/config/mise/config.toml ~/.config/mise/config.toml
+
+# nvim
+[ -d ~/.config/nvim ] || mkdir -p ~/.config/nvim
+ln -sfv $PWD/config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sfv $PWD/config/nvim/lua ~/.config/nvim/lua
+ln -sfv $PWD/config/nvim/sonictemplate ~/.config/nvim/sonictemplate
 

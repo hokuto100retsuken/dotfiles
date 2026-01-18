4cd0b82747af5783a18a52056234d21dc1e58840
hkt100rtkn <hokuto100retsuken@gmail.com>
2026-01-18 13:23:40 +0900

    feat(ghostty): add keybindings for pane management and window navigation in Neovim

--- config/nvim/lua/my/core/keymaps.lua
+++ config/nvim/lua/my/core/keymaps.lua
@@ -4,14 +4,25 @@
 local keymap = vim.keymap
 
 -- ┃ Window Management ┃
--- Split windows
+-- Neovimのウィンドウ分割と移動のキーマップ
+-- Ghosttyのキーマップ（ctrl+h/j/k/l, ctrl+w）と被らないように設定
+-- 標準的なVimのキーマップ（Ctrl+w + キー）も使用可能
+
+-- ウィンドウ分割（Neovim内でのみ有効、Ghosttyと被らない）
 keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true, desc = 'Split window horizontally' })
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true, desc = 'Split window vertically' })
--- Move between windows
-keymap.set('n', 'sh', '<C-w>h', { silent = true, desc = 'Move to left window' })
-keymap.set('n', 'sk', '<C-w>k', { silent = true, desc = 'Move to upper window' })
-keymap.set('n', 'sj', '<C-w>j', { silent = true, desc = 'Move to lower window' })
-keymap.set('n', 'sl', '<C-w>l', { silent = true, desc = 'Move to right window' })
+
+-- ウィンドウ間の移動
+-- 標準的なVimのキーマップ（Ctrl+w + h/j/k/l）は使用可能
+-- ただし、Ghosttyのctrl+h/j/k/lと被るため、Neovim内では以下のキーマップも使用可能
+keymap.set('n', '<leader>wh', '<C-w>h', { silent = true, desc = 'Move to left window' })
+keymap.set('n', '<leader>wj', '<C-w>j', { silent = true, desc = 'Move to lower window' })
+keymap.set('n', '<leader>wk', '<C-w>k', { silent = true, desc = 'Move to upper window' })
+keymap.set('n', '<leader>wl', '<C-w>l', { silent = true, desc = 'Move to right window' })
+
+-- ウィンドウを閉じる
+-- 標準的なVimのキーマップ（Ctrl+w q）も使用可能
+keymap.set('n', '<leader>wq', '<C-w>q', { silent = true, desc = 'Close window' })
 
 
 -- ┃ Applications ┃

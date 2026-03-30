a90d484af20b8f1d7512b57c82dc02d20dbba79e
hkt100rtkn <hokuto100retsuken@gmail.com>
2026-03-30 11:45:40 +0900

    paformance fishj

--- config/fish/conf.d/Darwin.fish
+++ config/fish/conf.d/Darwin.fish
@@ -1,23 +1,18 @@
 # macOS specific settings for fish shell
-if test (uname) = "Darwin"
-    # Add Homebrew to PATH
-    if test -d /opt/homebrew
-        # Apple Silicon Mac
-        set -gx PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
-        set -gx PATH /opt/homebrew/opt/git/bin $PATH
-    else if test -d /usr/local/Homebrew
-        # Intel Mac
-        set -gx PATH /usr/local/bin /usr/local/sbin $PATH
-        set -gx PATH /usr/local/opt/git/bin $PATH
-    end
+# /opt/homebrew はmacOS (Apple Silicon) 固有のパスなのでuname不要
+if test -d /opt/homebrew
+    set -gx PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
+    set -gx PATH /opt/homebrew/opt/git/bin $PATH
+else if test -d /usr/local/Homebrew
+    set -gx PATH /usr/local/bin /usr/local/sbin $PATH
+    set -gx PATH /usr/local/opt/git/bin $PATH
+else
+    return
+end
 
-    # mise
-    if command -v mise >/dev/null
-        mise activate fish | source
-    end
+# mise: Homebrew が vendor_conf.d/mise-activate.fish で自動activateするため不要
 
-    # zoxide
-    if command -v zoxide >/dev/null
-        zoxide init fish | source
-    end
+# zoxide
+if command -v zoxide >/dev/null
+    zoxide init fish | source
 end
